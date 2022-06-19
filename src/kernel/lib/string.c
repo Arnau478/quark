704e219c6cfb4ad15149136989a7c3634900f9c0
Arnau478 <arnauxabia@gmail.com>
2022-06-19 19:53:41 +0200

    Implemented strcmp()

--- src/kernel/lib/string.c
+++ src/kernel/lib/string.c
@@ -5,3 +5,8 @@ int strlen(char *s){
     while(*(s++)) len++;
     return len;
 }
+
+int strcmp(char *s1, char *s2){
+    while(*s1 && (*s1 == *s2)) {s1++; s2++;}
+    return *(const unsigned char *)s1 - *(const unsigned char *)s2;
+}
--- src/kernel/lib/string.h
+++ src/kernel/lib/string.h
@@ -1,3 +1,4 @@
 #pragma once
 
 int strlen(char *s);
+int strcmp(char *s1, char *s2);
