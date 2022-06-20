a59ad4dea18751cef780deb252790e085658721a
Arnau478 <arnauxabia@gmail.com>
2022-06-20 18:06:05 +0200

    Renamed commands to lowercase

--- src/kernel/shell.c
+++ src/kernel/shell.c
@@ -7,19 +7,19 @@ int shell_run(char *cmd){
     int ret = 0;
 
     puts("\x1b[32m");
-    if(!strcmp(cmd, "HELLO")){
+    if(!strcmp(cmd, "hello")){
         printf("Hello!\n");
     }
-    else if(!strcmp(cmd, "VERSION")){
+    else if(!strcmp(cmd, "version")){
         printf("%s\n", OS_VERSION);
     }
-    else if(!strcmp(cmd, "MALLOC")){
+    else if(!strcmp(cmd, "malloc")){
         printf("Allocated 10 bytes:\n%x\n", kmalloc(10));
     }
-    else if(!strcmp(cmd, "CALLOC")){
+    else if(!strcmp(cmd, "calloc")){
         printf("Allocated and zeroed 10 chars:\n%x\n", kcalloc(10, sizeof(char)));
     }
-    else if(!strcmp(cmd, "CLEAR")){
+    else if(!strcmp(cmd, "clear")){
         clear_screen();
     }
     else{
