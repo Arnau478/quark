5ed34dbbe65e80abb65598bdbfb52270a036d59f
Arnau478 <arnauxabia@gmail.com>
2022-06-20 14:17:13 +0200

    VERSION shell command

--- src/kernel/shell.c
+++ src/kernel/shell.c
@@ -3,11 +3,20 @@
 #include "lib/string.h"
 
 int shell_run(char *cmd){
+    int ret = 0;
+
+    puts("\x1b[32m");
     if(!strcmp(cmd, "HELLO")){
         printf("Hello!\n");
     }
+    else if(!strcmp(cmd, "VERSION")){
+        printf("%s\n", OS_VERSION);
+    }
     else{
         printf("SHELL: Unknown command \"%s\"\n", cmd);
-        return 127;
+        ret = 127;
     }
+    puts("\x1b[0m");
+    
+    return ret;
 }
